51edf77dfd476ffd8883dde96daf4fd6febd3013
Magetsu!! <148205878+magetsu002@users.noreply.github.com>
2026-08-21 16:16:01 +0400

    Add minimal Hyprland Lua config

---
+++ hypr-minimal.lua
@@ -0,0 +1,4 @@
+hl.bind("SUPER + RETURN", hl.dsp.exec_cmd("kitty"))
+hl.bind("SUPER + F", hl.dsp.exec_cmd("firefox"))
+hl.bind("SUPER + Q", hl.dsp.window.close())
+hl.bind("SUPER + M", hl.dsp.exit())
